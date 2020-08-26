4b2621bd82edc06bbeee290a9c398220bb7da5dc
wook2124 <wook2124@naver.com>
2020-08-26 19:32:15 +0900

    Update 001~010.py

--- 코드업 100제/001~010.py
+++ 코드업 100제/001~010.py
@@ -16,8 +16,12 @@
 
 # print("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518")
 
-# a = int(input())
+# a = int(input())      정수 1개 입력받아 그대로 출력하기
 # print('%d' %a)
 
-a = input()
-print(a)
+# a = input()       문자 1개 입력받아 그대로 출력하기
+# print(a)
+
+# a = float(input())        실수 1개 입력받아 그대로 출력하기
+# print('%f' %a)
+
