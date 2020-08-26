bcdcdf27af4f79180e746a4ad663defd9a07b8bc
wook2124 <wook2124@naver.com>
2020-08-26 20:28:11 +0900

    Create 001~020.py

---
+++ 코드업 100제/001~020.py
@@ -0,0 +1,47 @@
+# https://codeup.kr/problemset.php?search=%EA%B8%B0%EC%B4%88100%EC%A0%9C
+
+# print("Hello")
+
+# print("Hello World")
+
+# print("Hello\nWorld")
+
+# print("'Hello'")
+
+# print("\"Hello World\"")
+
+# print("\"!@#$%^&*()\"")
+
+# print("\"C:\Download\hello.cpp\"")
+
+# print("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518")
+
+# a = int(input())      정수 1개 입력받아
+# print(a)              그대로 출력하기
+
+# a = input()       문자 1개 입력받아
+# print(a)          그대로 출력하기
+
+# a = float(input())        실수 1개 입력받아
+# print(a)                  그대로 출력하기
+
+# a, b = input().split()            정수 2개 입력받아
+# print('%d %d' %(int(a), int(b)))  그대로 출력하기
+
+# a, b = input().split()        문자 2개 입력받아
+# print(b, a)                   순서 바꿔 출력하기
+
+# a = float(input())        실수 입력받아
+# print('%.2f' %a)          둘째 자리까지 출력하기
+
+# a = int(input())      정수 1개 입력받아
+# print(a, a, a)        3번 출력하기
+
+# h, m = input().split(':')           시간 입력받아
+# print('%d:%d' %(int(h), int(m)))    그대로 출력하기
+
+# y, m, d = input().split('.')                        연월일 입력받아
+# print('%04d.%02d.%02d' %(int(y), int(m), int(d)))   그대로 출력하기
+
+
+
