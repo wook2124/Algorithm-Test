8be42de1489c6c458a4c7bd735c560f2ce26c27b
wook2124 <wook2124@naver.com>
2020-08-26 19:45:02 +0900

    Update 001~010.py

--- 코드업 100제/001~010.py
+++ 코드업 100제/001~010.py
@@ -16,20 +16,24 @@
 
 # print("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518")
 
-# a = int(input())      정수 1개 입력받아 그대로 출력하기
-# print('%d' %a)
+# a = int(input())      정수 1개 입력받아
+# print('%d' %a)        그대로 출력하기
 
-# a = input()       문자 1개 입력받아 그대로 출력하기
-# print(a)
+# a = input()       문자 1개 입력받아
+# print(a)          그대로 출력하기
 
-# a = float(input())        실수 1개 입력받아 그대로 출력하기
-# print('%f' %a)
+# a = float(input())        실수 1개 입력받아
+# print('%f' %a)            그대로 출력하기
 
-# a, b = input().split()      정수 2개 입력받아 그대로 출력하기
-# print('%d %d' %(int(a), int(b)))
+# a, b = input().split()                정수 2개 입력받아 
+# print('%d %d' %(int(a), int(b)))      그대로 출력하기
 
-# a, b = input().split()        문자 2개 입력받아 순서 바꿔 출력하기
-# print(b, a)
+# a, b = input().split()        문자 2개 입력받아
+# print(b, a)                   순서 바꿔 출력하기
 
-a = float(input())
-print('%.2f' %a)
+# a = float(input())        실수 입력받아
+# print('%.2f' %a)          둘째 자리까지 출력하기
+
+# a = int(input())                  정수 1개 입력받아
+# print('%d %d %d' %(a, a, a))      3번 출력하기
+
