6384ef79b616a85bdefa94e4e11cf7ab09c7f669
wook2124 <wook2124@naver.com>
2020-08-27 12:00:44 +0900

    Update 021~040.py

--- 코드업 100제/021~040.py
+++ 코드업 100제/021~040.py
@@ -1,5 +1,6 @@
 # a = input()   단어 1개, 문장 1개 입력받아
 # print(a)      그대로 출력하기
 
-a, b = map(int, input().split('.'))
-print('%d\n%d' %(a, b))
+# a, b = map(int, input().split('.'))   실수 1개 입력받아
+# print('%d\n%d' %(a, b))               부분별로 출력하기
+
