fad86cf66807155759ea09a952ec633077c59e50
wook2124 <wook2124@naver.com>
2020-08-27 12:41:07 +0900

    Update 021~040.py

--- 코드업 100제/021~040.py
+++ 코드업 100제/021~040.py
@@ -4,3 +4,11 @@
 # a, b = map(int, input().split('.'))   실수 1개 입력받아
 # print('%d\n%d' %(a, b))               부분별로 출력하기
 
+# a = input()
+# for i in range(len(a)) :
+#    print("'" + a[i] + "'")
+
+
+# a = input()
+# for i in a :              단어 1개 입력받아
+#     print("'" + i + "'")  나누어 출력하기
