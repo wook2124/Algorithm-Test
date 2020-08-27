a98f53f9977b8b1813cf679035e8b14856223533
wook2124 <wook2124@naver.com>
2020-08-27 22:33:36 +0900

    Update 021~040.py

--- 코드업 100제/021~040.py
+++ 코드업 100제/021~040.py
@@ -1,21 +1,23 @@
-# a = input()   단어 1개, 문장 1개 입력받아
-# print(a)      그대로 출력하기
+# 1021 단어 1개 입력받아 그대로 출력하기
+# 1022 문장 1개 입력받아 그대로 출력하기
+# a = input()
+# print(a)
 
-
+# 1023 실수 1개 입력받아 부분별로 출력하기
 # a, b = map(int, input().split('.'))
-# print('%d\n%d' %(a, b))       실수 1개 입력받아        
+# print('%d\n%d' %(a, b))
 
-# a, b = input().split('.')     부분별로 출력하기     
+# a, b = input().split('.')
 # print(a + "\n" + b)
 
+# 1024 단어 1개 입력받아 나누어 출력하기
+# a = input()
+# for i in range(len(a)) :
+#    print("'" + a[i] + "'")
 
-# a = input()                   
-# for i in range(len(a)) :  
-#    print("'" + a[i] + "'")    단어 1개 입력받아
-
-# a = input()                   
-# for i in a :              
-#     print("'" + i + "'")      나누어 출력하기
+# a = input()
+# for i in a :
+#     print("'" + i + "'")
 
 # 1025 정수 1개 입력받아 나누어 출력하기
 # a = input()                 
@@ -24,9 +26,14 @@
 #     n = (len(a)-i-1)
 #     print([x*(10**n)])      
 
-# a, b, c, d, e = map(int, input()) 나누어 출력하기
+# a, b, c, d, e = map(int, input())
 # print('[%d0000]\n[%d000]\n[%d00]\n[%d0]\n[%d]' % (a, b, c, d, e))
 
+# 1026 시분초 입력받아 분만 출력하기
+# a, b, c = map(int, input().split(':'))
+# print(b)
 
-a, b, c = map(int, input().split(':'))
-print(b)
+# 1027 년월일 입력받아 형식 바꿔 출력하기
+# y, m, d = map(str, input().split("."))
+# print(d + "-" + m + "-" + y)
+
