068b6b086dd0a1dcd73d35d4979470dc7a04ba2f
wook2124 <wook2124@naver.com>
2020-08-27 13:29:36 +0900

    Update 021~040.py

--- 코드업 100제/021~040.py
+++ 코드업 100제/021~040.py
@@ -1,14 +1,23 @@
 # a = input()   단어 1개, 문장 1개 입력받아
 # print(a)      그대로 출력하기
 
-# a, b = map(int, input().split('.'))   실수 1개 입력받아
-# print('%d\n%d' %(a, b))               부분별로 출력하기
 
-# a = input()
-# for i in range(len(a)) :
-#    print("'" + a[i] + "'")
+# a, b = map(int, input().split('.'))
+# print('%d\n%d' %(a, b))       실수 1개 입력받아        
+
+# a, b = input().split('.')     부분별로 출력하기     
+# print(a + "\n" + b)
 
 
-# a = input()
-# for i in a :              단어 1개 입력받아
-#     print("'" + i + "'")  나누어 출력하기
+# a = input()                   
+# for i in range(len(a)) :  
+#    print("'" + a[i] + "'")    단어 1개 입력받아
+
+
+# a = input()                   
+# for i in a :              
+#     print("'" + i + "'")      나누어 출력하기
+
+
+a, b = "1.414213".split('.')
+print(a + "\n" + b)
