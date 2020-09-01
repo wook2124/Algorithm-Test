43b69a684076a28d485480552b8f428aac44a62a
wook2124 <wook2124@naver.com>
2020-09-02 01:55:58 +0900

    Update 021~040.py

--- 코드업 100제/021~040.py
+++ 코드업 100제/021~040.py
@@ -116,3 +116,12 @@ print(a % b)
 # 1044 : [기초-산술연산] 정수 1개 입력받아 1 더해 출력하기
 a = int(input())
 print(a + 1)
+
+# 1045 : [기초-산술연산] 정수 2개 입력받아 자동 계산하기
+a, b = map(int, input().split(" "))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+print('%0.2f' %(a / b))
