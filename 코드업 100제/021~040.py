8d51a6f50daf4945f4bb507d113d005b2ea27c77
wook2124 <wook2124@naver.com>
2020-09-02 01:52:19 +0900

    Update 021~040.py

--- 코드업 100제/021~040.py
+++ 코드업 100제/021~040.py
@@ -108,3 +108,7 @@ print(chr(ord(a) + 1))
 # 1042 : [기초-산술연산] 정수 2개 입력받아 나눈 몫 출력하기
 a, b = map(int, input().split(" "))
 print(a // b)
+
+# 1043 : [기초-산술연산] 정수 2개 입력받아 나눈 나머지 출력하기
+a, b = map(int, input().split(" "))
+print(a % b)
