f2c5b00b43ddd6beb7c513711695ce5d1440bcdd
wook2124 <wook2124@naver.com>
2020-09-06 23:07:00 +0900

    Update 065~.py

--- 코드업 100제/065~.py
+++ 코드업 100제/065~.py
@@ -6,3 +6,18 @@ if b % 2 == 0:
     print(b)
 if c % 2 == 0:
     print(c)
+
+# 1066 : [기초-조건/선택실행구조] 정수 3개 입력받아 짝/홀 출력하기
+a, b, c = map(int, input().split(" "))
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
+if b % 2 == 0:
+    print("even")
+else:
+    print("odd")
+if c % 2 == 0:
+    print("even")
+else:
+    print("odd")
