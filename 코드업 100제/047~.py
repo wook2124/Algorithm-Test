51b57c0bcdbb77173b92b778d904ff2a86f568a1
wook2124 <wook2124@naver.com>
2020-09-06 16:35:27 +0900

    Update 047~.py

--- 코드업 100제/047~.py
+++ 코드업 100제/047~.py
@@ -35,3 +35,11 @@ if b >= a:
     print(1)
 else:
     print(0)
+
+# 1052 : [기초-비교연산] 두 정수 입력받아 비교하기4
+# 두 정수(a, b)를 입력받아 a와 b가 서로 다르면 1을, 그렇지 않으면 0을 출력하는 프로그램을 작성해보자.
+a, b = map(int, input().split(" "))
+if a != b:
+    print(1)
+else:
+    print(0)
