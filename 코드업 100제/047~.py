b2a9a9a779fa709c1fb7f4188765bb5a367466a7
wook2124 <wook2124@naver.com>
2020-09-06 16:37:04 +0900

    Update 047~.py

--- 코드업 100제/047~.py
+++ 코드업 100제/047~.py
@@ -43,3 +43,12 @@ if a != b:
     print(1)
 else:
     print(0)
+
+# 1053 : [기초-논리연산] 참 거짓 바꾸기
+# 1(true, 참) 또는 0(false, 거짓) 이 입력되었을 때 반대로 출력하는 프로그램을 작성해보자.
+a = int(input())
+if a == 1:
+    print(0)
+else:
+    print(1)
+
