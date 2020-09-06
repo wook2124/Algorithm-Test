4e18fe7f81390f1dbb70f585b141176d0447fb4f
wook2124 <wook2124@naver.com>
2020-09-06 23:14:21 +0900

    Update 065~.py

--- 코드업 100제/065~.py
+++ 코드업 100제/065~.py
@@ -50,3 +50,23 @@ elif a >= 40:
     print("C")
 else:
     print("D")
+
+# 1069 : [기초-조건/선택실행구조] 평가 입력받아 다르게 출력하기
+# 평가 내용
+# 평가 : 내용
+# A : best!!!
+# B : good!!
+# C : run!
+# D : slowly~
+# 나머지 문자들 : what?
+a = input()
+if a == "A":
+    print("best!!!")
+elif a == "B":
+    print("good!!")
+elif a == "C":
+    print("run!")
+elif a =="D":
+    print("slowly~")
+else:
+    print("what?")
