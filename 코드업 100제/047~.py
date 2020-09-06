8c35cef3244dfc17ed482ed237717441b50fd733
wook2124 <wook2124@naver.com>
2020-09-06 16:47:13 +0900

    Update 047~.py

--- 코드업 100제/047~.py
+++ 코드업 100제/047~.py
@@ -78,3 +78,10 @@ if a == b:
     print(1)
 else:
     print(0)
+
+# 1058 : [기초-논리연산] 둘 다 거짓일 경우만 참 출력하기
+a, b = map(int, input().split(" "))
+if a == 0 and b == 0:
+    print(1)
+else:
+    print(0)
