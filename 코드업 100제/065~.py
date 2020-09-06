823840970adf21eb3917ca6836361c8eb4c19a72
wook2124 <wook2124@naver.com>
2020-09-06 23:19:00 +0900

    Update 065~.py

--- 코드업 100제/065~.py
+++ 코드업 100제/065~.py
@@ -35,7 +35,6 @@ else:
     print("odd")
 
 # 1068 : [기초-조건/선택실행구조] 정수 1개 입력받아 평가 출력하기
-# 평가 기준
 # 점수 범위 : 평가
 # 90 ~ 100 : A
 # 70 ~   89 : B
@@ -52,7 +51,6 @@ else:
     print("D")
 
 # 1069 : [기초-조건/선택실행구조] 평가 입력받아 다르게 출력하기
-# 평가 내용
 # 평가 : 내용
 # A : best!!!
 # B : good!!
@@ -70,3 +68,19 @@ elif a =="D":
     print("slowly~")
 else:
     print("what?")
+
+# 1070 : [기초-조건/선택실행구조] 월 입력받아 계절 출력하기
+# 월 : 계절 이름
+# 12, 1, 2 : winter
+#  3, 4, 5 : spring
+#  6, 7, 8 : summer
+#  9, 10, 11 : fall
+a = int(input())
+if a in(12, 1, 2):
+    print("winter")
+elif a in(3, 4, 5):
+    print("spring")
+elif a in(6, 7, 8):
+    print("summer")
+else:
+    print("fall")
