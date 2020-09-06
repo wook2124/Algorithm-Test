04a6e9db7d32d8c419771403cfa12b220271b6a7
wook2124 <wook2124@naver.com>
2020-09-06 16:21:30 +0900

    Update 047~.py

--- 코드업 100제/047~.py
+++ 코드업 100제/047~.py
@@ -1,7 +1,13 @@
 # 1047 : [기초-비트시프트연산] 정수 1개 입력받아 2배 곱해 출력하기
 a = input()
-print(a*2)
+print(a * 2)
 
 a = int(input())
-print(a<<1)
+print(a << 1)
 
+# 1048 : [기초-비트시프트연산] 한 번에 2의 거듭제곱 배로 출력하기
+a, b = map(int, input().split(" "))
+print(a * (2 ** b))
+
+a, b = map(int, input().split(" "))
+print(a << b)
