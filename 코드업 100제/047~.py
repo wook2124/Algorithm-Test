e94805cdc19e2bbb592cda10940d4ab8d607a0a3
wook2124 <wook2124@naver.com>
2020-09-06 16:44:46 +0900

    Update 047~.py

--- 코드업 100제/047~.py
+++ 코드업 100제/047~.py
@@ -64,3 +64,11 @@ if a | b == 1:
     print(1)
 else:
     print(0)
+
+# 1056 : [기초-논리연산] 참/거짓이 서로 다를 때에만 참 출력하기
+a, b = map(int, input().split(" "))
+if a != b:
+    print(1)
+else:
+    print(0)
+
