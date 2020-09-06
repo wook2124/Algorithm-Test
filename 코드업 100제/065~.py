3cec0930b0cfc3dd46bbb48143126fb0656e22ed
wook2124 <wook2124@naver.com>
2020-09-06 23:09:10 +0900

    Update 065~.py

--- 코드업 100제/065~.py
+++ 코드업 100제/065~.py
@@ -21,3 +21,15 @@ if c % 2 == 0:
     print("even")
 else:
     print("odd")
+
+# 1067 : [기초-조건/선택실행구조] 정수 1개 입력받아 분석하기
+# 정수 1개가 입력되었을 때, 음(minus)/양(plus)과 짝(even)/홀(odd)을 출력해보자.
+a = int(input())
+if a < 0:
+    print("minus")
+else:
+    print("plus")
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
