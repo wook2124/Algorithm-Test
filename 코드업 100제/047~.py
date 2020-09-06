ee67ee2a969bd84c24eccfeea51fb12d022ea31d
wook2124 <wook2124@naver.com>
2020-09-06 16:30:09 +0900

    Update 047~.py

--- 코드업 100제/047~.py
+++ 코드업 100제/047~.py
@@ -11,3 +11,11 @@ print(a * (2 ** b))
 
 a, b = map(int, input().split(" "))
 print(a << b)
+
+# 1049 : [기초-비교연산] 두 정수 입력받아 비교하기1
+# 두 정수(a, b)를 입력받아 a가 b보다 크면 1을, a가 b보다 작거나 같으면 0을 출력하는 프로그램을 작성해보자.
+a, b = map(int, input().split(" "))
+if a > b:
+    print(1)
+else: 
+    print(0)
