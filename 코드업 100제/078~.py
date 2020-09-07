aec0e08806104542f7382db95a3a698e8dea1181
wook2124 <wook2124@naver.com>
2020-09-08 02:19:21 +0900

    Update 078~.py

--- 코드업 100제/078~.py
+++ 코드업 100제/078~.py
@@ -112,3 +112,10 @@ for i in range(n - 1):
     a *= m
     a += d
 print(a)
+
+# 1092 : [기초-종합] 함께 문제 푸는 날
+a, b, c = map(int, input().split())
+day = 1
+while day % a != 0 or day % b != 0 or day % c != 0:
+    day += 1
+print(day)
