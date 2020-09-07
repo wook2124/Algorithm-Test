a570b83baae67a1b570b019a1815df155e2de5c0
wook2124 <wook2124@naver.com>
2020-09-08 02:06:56 +0900

    Update 078~.py

--- 코드업 100제/078~.py
+++ 코드업 100제/078~.py
@@ -104,3 +104,11 @@ a, r, n = map(int, input().split(" "))
 for i in range(n - 1):
     a *= r
 print(a)
+
+# 1091 : [기초-종합] 수 나열하기3
+# 시작 값(a), 곱할 값(m), 더할 값(d), 몇 번째 인지를 나타내는 정수(n)가 공백을 두고 입력된다.(a, m, d는 -50 ~ +50, n은 10이하의 자연수)
+a, m, d, n = map(int, input().split(" "))
+for i in range(n - 1):
+    a *= m
+    a += d
+print(a)
