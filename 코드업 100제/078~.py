430d3d13ae1be2c3c0f5086f196763fe6e1bd827
wook2124 <wook2124@naver.com>
2020-09-08 01:30:42 +0900

    Update 078~.py

--- 코드업 100제/078~.py
+++ 코드업 100제/078~.py
@@ -21,3 +21,9 @@ for i in range(a + 1):
     if n >= a:
         print(i)
         break
+
+# 1081 : [기초-종합] 주사위를 2개 던지면?
+n, m = map(int, input().split(" "))
+for i in range(1, n + 1):
+    for j in range (1, m + 1):
+        print(i, j)
