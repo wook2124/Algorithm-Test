74639a91e4565ff6ca0fb68d808e6c7c51f0bc8a
wook2124 <wook2124@naver.com>
2020-09-08 01:48:50 +0900

    Update 078~.py

--- 코드업 100제/078~.py
+++ 코드업 100제/078~.py
@@ -51,3 +51,14 @@ for i in range(r):
             cnt += 1
             print(i, j, k)
 print(cnt)
+
+# 1085 : [기초-종합] 소리 파일 저장용량 계산하기
+# h, b, c, s 가 공백을 두고 입력된다.
+# h는 48,000이하, b는 32이하(단, 8의배수), c는 5이하, s는 6,000이하의 자연수이다.
+h, b, c, s = map(int, input().split(" "))
+sum = h * b * c * s
+result = sum / (8 * (2 ** 20))
+print("%0.1f MB" %result)
+
+h, b, c, s = map(int, input().split(" "))
+print('%0.1f MB' % (h * b * c * s / 1024 / 1024 / 8))
