ff99832925e50ed4b49add37732f8c0f7145c5cb
wook2124 <wook2124@naver.com>
2020-09-08 01:10:55 +0900

    Update 078~.py

--- 코드업 100제/078~.py
+++ 코드업 100제/078~.py
@@ -5,3 +5,10 @@ for i in range(1, a + 1):
     if i % 2 == 0:
         n += i
 print(n)
+
+# 1079 : [기초-종합] 원하는 문자가 입력될 때까지 반복 출력하기
+a = input().split()
+for i in a:
+    print(i)
+    if i == "q":
+        break
