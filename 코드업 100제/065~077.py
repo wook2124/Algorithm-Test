b1c5e00a4ba1f0cf73b2bb2b8c51524129c9fc70
wook2124 <wook2124@naver.com>
2020-09-09 22:39:21 +0900

    Update 065~077.py

--- 코드업 100제/065~077.py
+++ 코드업 100제/065~077.py
@@ -1,5 +1,5 @@
 # 1065 : [기초-조건/선택실행구조] 정수 3개 입력받아 짝수만 출력하기
-a, b, c = map(int, input().split(" "))
+a, b, c = map(int, input().split())
 if a % 2 == 0:
     print(a)
 if b % 2 == 0:
@@ -8,7 +8,7 @@ if c % 2 == 0:
     print(c)
 
 # 1066 : [기초-조건/선택실행구조] 정수 3개 입력받아 짝/홀 출력하기
-a, b, c = map(int, input().split(" "))
+a, b, c = map(int, input().split())
 if a % 2 == 0:
     print("even")
 else:
