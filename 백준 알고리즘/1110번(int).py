abf1456ce93552a07a2d15e14c9d1d99f1dc9ed1
wook2124 <wook2124@naver.com>
2020-09-11 22:11:14 +0900

    Update 1110번(int).py

--- 백준 알고리즘/1110번(int).py
+++ 백준 알고리즘/1110번(int).py
@@ -7,16 +7,16 @@ N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작
 '''
 n = int(input())         # 68
 num = n
-cnt = 0                  # 싸이클 수
+cnt = 0                  # 사이클 수
 
-while True:
+while True:              # while 1이랑 동일
     a = num // 10        # 6
     b = num % 10         # 8
     c = (a + b) % 10     # 6 + 8 = 1"4"
-    num = (b * 10) + c   # 8 + 4 = 84
+    num = (b * 10) + c   # 80 + 4 = 84
 
-    cnt = cnt + 1        # 싸이클 수 + 1
-    if(num == n):        # ls에서 68 숫자가 나오면 멈춤
+    cnt = cnt + 1        # 사이클 수 + 1
+    if(num == n):        # num에서 입력된 n과 똑같은 숫자(68)가 나오면 멈춤
         break
 
 print(cnt)
