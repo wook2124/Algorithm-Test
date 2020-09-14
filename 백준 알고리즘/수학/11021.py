eed92b208971149896690614085aff94299ad680
wook2124 <wook2124@naver.com>
2020-09-15 02:38:43 +0900

    Delete 11021.py

--- 백준 알고리즘/수학/11021.py
+++
@@ -1,7 +0,0 @@
-# 11021번 : A + B - 7
-t = int(input())        # 5
-
-for i in range(t):      # i = 0 ~ 4
-    i += 1              # i = 1 ~ 5
-    a, b = map(int, input().split())
-    print("Case #%s: %s" %(i, a + b))
