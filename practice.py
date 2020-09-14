82350a6ca6115ff2a61a85663ee99f04b2b1861f
wook2124 <wook2124@naver.com>
2020-09-14 23:11:17 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,6 +1,4 @@
-while 1:
-    a, b = map(int, input().split())
-    if (a == 0 and b == 0):
-        break
-    else:
-        print(a + b)
+s = 'show how to index into sequences'.split()
+full_slice = s
+
+print(full_slice)
