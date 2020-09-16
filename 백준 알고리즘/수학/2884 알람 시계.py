19403f99f4827652703a80062e2461199ec29f80
wook2124 <wook2124@naver.com>
2020-09-17 01:57:00 +0900

    Update 2884 알람 시계.py

--- 백준 알고리즘/수학/2884 알람 시계.py
+++ 백준 알고리즘/수학/2884 알람 시계.py
@@ -1,1 +1,9 @@
 # 2884번 : 알람 시계
+h,m = map(int, input().split())
+
+if m > 44:                  # 10h 45m
+    print(h, m - 45)        # 10h 0m
+elif h >= 1 and m <= 44:    # 23h 40m, 1h 5m
+    print(h - 1, m + 15)    # 22h 55m, 1h 20m
+else:                       # 0h 30m
+    print(23, m + 15)       # 23h 45m
