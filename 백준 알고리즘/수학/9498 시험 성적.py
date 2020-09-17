11a8a87cfb1d1371077ee31a9837914917fe04d5
wook2124 <wook2124@naver.com>
2020-09-18 00:28:06 +0900

    Create 9498 시험 성적.py

---
+++ 백준 알고리즘/수학/9498 시험 성적.py
@@ -0,0 +1,13 @@
+# 9498번 : 시험 성적
+score = int(input())
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
