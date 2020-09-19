d920098da8c4471255909bad99a7e73535a3b838
wook2124 <wook2124@naver.com>
2020-09-19 18:20:16 +0900

    Update 4673 셀프 넘버.py

--- 백준 알고리즘/구현/4673 셀프 넘버.py
+++ 백준 알고리즘/구현/4673 셀프 넘버.py
@@ -4,7 +4,7 @@ generated_num = set()
 
 for i in range(1, 10001):       # i = 850       
     for j in str(i):            # j = "8", "5", "0"
-        i += int(j)             # 850 + 8 + 5+ 0, i = 863
+        i += int(j)             # 850 + 8 + 5 + 0, i = 863
     generated_num.add(i)        # 생성자가 있는 숫자들
 
 self_num = sorted(natural_num - generated_num)
