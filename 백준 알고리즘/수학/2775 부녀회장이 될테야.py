7b3a3b8178ec580f868d41d7512e4989c8af77cc
wook2124 <wook2124@naver.com>
2020-09-23 10:42:44 +0900

    Update 2775 부녀회장이 될테야.py

--- 백준 알고리즘/수학/2775 부녀회장이 될테야.py
+++ 백준 알고리즘/수학/2775 부녀회장이 될테야.py
@@ -1,1 +1,11 @@
 # 2775번 : 부녀회장이 될테야
+t = int(input())                        # t = 2
+
+for _ in range(t):
+    k = int(input())                    # k = 1, 2
+    n = int(input())                    # n = 3, 3
+    num = [i for i in range(1, n + 1)]  # [1, 2, 3], [1, 2, 3]
+    for _ in range(k):
+        for i in range(1, n):
+            num[i] += num[i - 1]        # [1, 3, 6] -> [1, 4, 10]
+    print(num[-1])
