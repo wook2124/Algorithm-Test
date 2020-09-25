7be1b2ad000fcee2d46aba1e6e2a201419d94c5d
wook2124 <wook2124@naver.com>
2020-09-25 16:12:27 +0900

    Create 1003 피보나치 함수.py

---
+++ 백준 알고리즘/다이나믹 프로그래밍/1003 피보나치 함수.py
@@ -0,0 +1,13 @@
+# 1003번 : 피보나치 함수
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    zero = 1
+    one = 0
+    tmp = 0
+    for _ in range(n):
+        tmp = one
+        one = one + zero
+        zero = tmp
+    print(zero, one)
