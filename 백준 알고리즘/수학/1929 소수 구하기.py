a634b1992fad3c9669389b42708fd2efcba80b3a
wook2124 <wook2124@naver.com>
2020-10-06 02:06:14 +0900

    Create 1929 소수 구하기.py

---
+++ 백준 알고리즘/수학/1929 소수 구하기.py
@@ -0,0 +1,17 @@
+# 1929번 : 소수 구하기
+def prime(num):
+    if num < 2:
+        return False
+    
+    i = 2
+    while i * i <= num:
+        if num % i == 0:
+            return False
+        i += 1
+    return True
+
+m, n = map(int, input().split())
+
+for i in range(m, n + 1):
+    if prime(i):
+        print(i)
