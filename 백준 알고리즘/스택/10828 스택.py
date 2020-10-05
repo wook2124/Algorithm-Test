c881a7160a6a6ebede56557df52083ea34fec1a4
wook2124 <wook2124@naver.com>
2020-10-05 13:27:14 +0900

    Update 10828 스택.py

--- 백준 알고리즘/스택/10828 스택.py
+++ 백준 알고리즘/스택/10828 스택.py
@@ -1,1 +1,32 @@
 # 10828번 : 스택
+import sys
+n = int(sys.stdin.readline())
+stack = []
+
+for _ in range(n):
+    i = sys.stdin.readline().split()
+
+    if i[0] == 'push':
+        stack.append(int(i[1]))
+    
+    elif i[0] == 'pop':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
+            stack.pop()
+    
+    elif i[0] =='size':
+        print(len(stack))
+
+    elif i[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif i[0] == 'top':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
