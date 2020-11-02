c3472f9f02dc4d2ca01c724a3dce44361537c21e
wook2124 <wook2124@naver.com>
2020-11-03 01:34:27 +0900

    Update 1978 소수 찾기.py

--- 백준 알고리즘/수학/1978 소수 찾기.py
+++ 백준 알고리즘/수학/1978 소수 찾기.py
@@ -1,7 +1,13 @@
 # 1978번 : 소수 찾기
 n = int(input())
-ls = []
+ls = list(map(int, input().split()))
+count = 0
 
-for _ in range(n):
-    ls.append()
-
+for i in ls:
+    if i == 1:
+        continue
+    
+    if i % 2 != 0:
+        count += 1
+    
+print(count)
