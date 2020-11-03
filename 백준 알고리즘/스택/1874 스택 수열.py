47e85095328d402bab90e4060746b0ed9f67d50a
wook2124 <wook2124@naver.com>
2020-11-04 01:29:48 +0900

    Update 1874 스택 수열.py

--- 백준 알고리즘/스택/1874 스택 수열.py
+++ 백준 알고리즘/스택/1874 스택 수열.py
@@ -1,1 +1,27 @@
 # 1874번 : 스택 수열
+n = int(input())
+stack = []
+result = []
+count = 0
+X = True
+
+for _ in range(n):
+    num = int(input())
+
+    while count < num:
+        count += 1
+        stack.append(count)
+        result.append("+")
+
+    if stack[-1] == num:
+        stack.pop()
+        result.append("-")
+    else:
+        X = False
+        break
+
+if X == False:
+    print("NO")
+else:
+    for i in result:
+        print(i)
