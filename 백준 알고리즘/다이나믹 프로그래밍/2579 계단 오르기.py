65bbd3f4906b1f7bb0a2280a906d1aa1a123d8cf
wook2124 <wook2124@naver.com>
2020-11-28 23:18:03 +0900

    Update 2579 계단 오르기.py

--- 백준 알고리즘/다이나믹 프로그래밍/2579 계단 오르기.py
+++ 백준 알고리즘/다이나믹 프로그래밍/2579 계단 오르기.py
@@ -11,3 +11,8 @@ for _ in range(i):
 
 dp.append(stair[0])
 dp.append(max(stair[0] + stair[1], stair[1]))
+dp.append(max(stair[0] + stair[2], stair[1] + stair[2]))
+for i in range(3, n):
+    dp.append(max(dp[i - 2] + stair[i], dp[i - 3] + stair[i] + stair[i - 1]))
+
+print(dp.pop())
