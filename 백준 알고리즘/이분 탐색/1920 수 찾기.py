7cb02bf48c3bd9aa9bdd9aa9fd46da5f720fa01a
wook2124 <wook2124@naver.com>
2020-12-04 17:15:06 +0900

    Update 1920 수 찾기.py

--- 백준 알고리즘/이분 탐색/1920 수 찾기.py
+++ 백준 알고리즘/이분 탐색/1920 수 찾기.py
@@ -1,4 +1,5 @@
 # 1920번 : 수 찾기
+"""
 n = int(input())
 N = set(map(int, input().split()))
 
@@ -6,3 +7,18 @@ m = int(input())
 M = set(map(int, input().split()))
 
 result = N & M
+
+print(result)
+"""
+import sys
+
+n = int(sys.stdin.readline())
+N = set(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+M = list(map(int, sys.stdin.readline().split()))
+
+for i in M:
+    if i in N:
+        print(1)
+    else:
+        print(0)
