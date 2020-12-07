7dbd3c04ea931deedd103e979add3253dacd27fe
wook2124 <wook2124@naver.com>
2020-12-07 18:43:47 +0900

    Update 11726 2 x n 타일링.py

--- 백준 알고리즘/다이나믹 프로그래밍/11726 2 x n 타일링.py
+++ 백준 알고리즘/다이나믹 프로그래밍/11726 2 x n 타일링.py
@@ -1,1 +1,15 @@
 # 11726번 : 2 x n 타일링
+n = int(input())
+
+# [0, 0, 0, 0 ...]
+dp = [0 for _ in range(n + 1)]
+
+if n <= 3:
+    print(n)
+else:
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    print(dp[i] % 10007)
