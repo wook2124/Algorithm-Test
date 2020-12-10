1c1eb15d923be94fa14f0b5f8be0a00ce3afa133
wook2124 <wook2124@naver.com>
2020-12-11 01:55:17 +0900

    Update 13458 시험 감독.py

--- 백준 알고리즘/수학/13458 시험 감독.py
+++ 백준 알고리즘/수학/13458 시험 감독.py
@@ -1,1 +1,15 @@
 # 13458번 : 시험 감독
+n = int(input())
+tester = list(map(int, input().split()))
+a, b = map(int, input().split())
+
+cnt = n
+for i in tester:
+    i -= a
+    if i > 0:
+        if i % b:
+            cnt += (i // b + 1)
+        else:
+            cnt += (i // b)
+
+print(cnt)
