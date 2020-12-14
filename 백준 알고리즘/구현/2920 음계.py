9fc7ad73710c5032c2a42f573ebeccbad9baebdc
wook2124 <wook2124@naver.com>
2020-12-14 11:12:09 +0900

    Create 2920 음계.py

---
+++ 백준 알고리즘/구현/2920 음계.py
@@ -0,0 +1,9 @@
+# 2920번 : 음계
+scale = list(map(int, input().split()))
+
+if scale == sorted(scale):
+    print("ascending")
+elif scale == sorted(scale, reverse = True):
+    print("descending")
+else:
+    print("mixed")
