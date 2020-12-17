e7b8f695b0a8a9d777e0ef9c107b71f6aae29c6e
wook2124 <wook2124@naver.com>
2020-12-18 05:21:52 +0900

    Update 11651 좌표 정렬하기 2.py

--- 백준 알고리즘/정렬/11651 좌표 정렬하기 2.py
+++ 백준 알고리즘/정렬/11651 좌표 정렬하기 2.py
@@ -1,11 +1,21 @@
 # 11651번 : 좌표 정렬하기 2
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 array = []
 for i in range(n):
-    a, b = map(int, input().split())
-    array.append([b, a])
+    x, y = map(int, input().split())
+    array.append([y, x])
 
 s_array = sorted(array)
 
-print(s_array)
+for y, x in s_array:
+    print(x, y)
+
+"""
+# 두번째 출력방법
+for i in range(n):
+    print(s_array[i][1], s_array[i][0])
+"""
