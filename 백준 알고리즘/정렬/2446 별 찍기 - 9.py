d6f58081aa090f291a65a4ce2c450c9dca1742c9
wook2124 <wook2124@naver.com>
2020-12-29 00:00:44 +0900

    Update 2446 별 찍기 - 9.py

--- 백준 알고리즘/정렬/2446 별 찍기 - 9.py
+++ 백준 알고리즘/정렬/2446 별 찍기 - 9.py
@@ -4,7 +4,5 @@ n = int(input())
 for i in range(1, n + 1):
     print(" " * (i - 1) + "*" * (2 * (n - i) + 1))
 
-'''
 for j in range(1, n):
-    print(" " * (n - j - 1) + "*" * (2 * j + 1))
-'''
+    print(" " * (n - j - 1) + "*" * ((2 * j) + 1))
